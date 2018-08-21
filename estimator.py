41931a302faa52180e7e118424c0232721daae22
mckib2 <nicholas.bgp@gmail.com>
2018-08-21 14:27:30 -0600

    Save result indices.

--- estimator.py
+++ estimator.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf 
+import tensorflow as tf
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -22,29 +22,29 @@ data = Data(percent_load = 0.02)
 from model import unet
 
 # Training Parameters
-learning_rate = 0.0001 
+learning_rate = 0.0001
 num_steps = 20000
-batch_size = 32 
+batch_size = 32
 display_step = 1000
 
-# Network Parameters 
+# Network Parameters
 WIDTH = 256; HEIGHT = 256; CHANNELS = 2
 NUM_INPUTS = WIDTH * HEIGHT * CHANNELS
-NUM_OUTPUTS = 2 
+NUM_OUTPUTS = 2
 
 # Network Varibles and placeholders
 X = tf.placeholder(tf.float32, [None, HEIGHT, WIDTH, CHANNELS])  # Input
 Y = tf.placeholder(tf.float32, [None, HEIGHT, WIDTH, NUM_OUTPUTS]) # Truth Data - Output
 
 # Define loss and optimizer
-logits, _ = unet(X, NUM_OUTPUTS) 
+logits, _ = unet(X, NUM_OUTPUTS)
 
 prediction = logits
-loss = tf.reduce_mean(tf.square(prediction - Y)) 
+loss = tf.reduce_mean(tf.square(prediction - Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 trainer = optimizer.minimize(loss)
 
-# Initalize varibles, and run network 
+# Initalize varibles, and run network
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
@@ -61,7 +61,7 @@ for step in range(num_steps):
     sess.run( trainer, feed_dict={ X: batch_xs, Y: batch_ys } )
 
     if(step % display_step == 0):
-        train_loss = sess.run(loss, feed_dict={ X: batch_xs, Y: batch_ys })   
+        train_loss = sess.run(loss, feed_dict={ X: batch_xs, Y: batch_ys })
         test_loss = sess.run(loss, feed_dict={ X: data.x_test, Y: data.y_test })
         _step.append(step)
         _train_loss.append(train_loss)
@@ -81,7 +81,7 @@ plt.title("Loss for reordering")
 plt.savefig('results/loss.png')
 
 # Show results
-index = 0 
+index = 0
 true_reorder = data.denormalize(batch_ys[index, :, :, 1].squeeze(), data.y_min, data.y_max)
 new_reorder = data.denormalize(pred_train[index, :, :, 1].squeeze(), data.y_min, data.y_max)
 diff = np.abs(true_reorder - new_reorder)
@@ -89,13 +89,17 @@ true_reorder_test = data.denormalize(data.y_test[index, :, :, 1].squeeze(), data
 new_reorder_test = data.denormalize(pred_test[index, :, :, 1].squeeze(), data.y_min, data.y_max)
 diff_test = np.abs(true_reorder - new_reorder)
 
-matplotlib.image.imsave('results/true_reorder.png', true_reorder, cmap='gray', vmin=0, vmax=255) 
-matplotlib.image.imsave('results/new_reorder.png', new_reorder, cmap='gray', vmin=0, vmax=255) 
-matplotlib.image.imsave('results/diff_reorder.png', diff, cmap='gray', vmin=0, vmax=255) 
+# Save the new reordering for comparison
+np.save('results/new_reorder_test.npy',new_reorder_test)
+np.save('results/new_reorder.npy',new_reorder)
 
-matplotlib.image.imsave('results/true_reorder_test.png', true_reorder_test, cmap='gray', vmin=0, vmax=255) 
-matplotlib.image.imsave('results/new_reorder_test.png', new_reorder_test, cmap='gray', vmin=0, vmax=255) 
-matplotlib.image.imsave('results/diff_reorder_test.png', diff_test, cmap='gray', vmin=0, vmax=255) 
+matplotlib.image.imsave('results/true_reorder.png', true_reorder, cmap='gray', vmin=0, vmax=255)
+matplotlib.image.imsave('results/new_reorder.png', new_reorder, cmap='gray', vmin=0, vmax=255)
+matplotlib.image.imsave('results/diff_reorder.png', diff, cmap='gray', vmin=0, vmax=255)
+
+matplotlib.image.imsave('results/true_reorder_test.png', true_reorder_test, cmap='gray', vmin=0, vmax=255)
+matplotlib.image.imsave('results/new_reorder_test.png', new_reorder_test, cmap='gray', vmin=0, vmax=255)
+matplotlib.image.imsave('results/diff_reorder_test.png', diff_test, cmap='gray', vmin=0, vmax=255)
 
 plt.subplot(2,3,1)
 plt.imshow(true_reorder, cmap='gray', vmin=0, vmax=255)
@@ -109,4 +113,4 @@ plt.subplot(2,3,5)
 plt.imshow(new_reorder_test, cmap='gray', vmin=0, vmax=255)
 plt.subplot(2,3,6)
 plt.imshow(diff_test, cmap='gray', vmin=0, vmax=255)
-plt.show() 
+plt.show()
